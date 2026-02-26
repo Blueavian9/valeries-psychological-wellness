4e36892c6ada225242f6d1798a706b982f0fd42f
Cesar Aguilar <Blueavian9@gmail.com>
2026-02-26 14:34:11 -0800

    updated scripts path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// Fix for Windows esbuild binary path
+import { join } from 'path'
+process.env.ESBUILD_BINARY_PATH = join(
+  process.cwd(),
+  'node_modules/@esbuild/win32-x64/esbuild.exe'
+)
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
